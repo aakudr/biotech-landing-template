48448a805494808f6ea2f752950c421896ad7f6b
eternity <aakudr2k3@gmail.com>
2023-06-30 20:15:26 +0600

    fix molecule canvas placement

--- molecule.js
+++ molecule.js
@@ -17,9 +17,10 @@ const camera = new THREE.PerspectiveCamera(
 )
 camera.position.z = 3
 
-const renderer = new THREE.WebGLRenderer()
+const renderer = new THREE.WebGLRenderer( { antialias: true, alpha: true })
 renderer.setSize( window.innerWidth, window.innerHeight )
-document.body.appendChild( renderer.domElement )
+renderer.setClearColor( 0x000000, 0 ); // the default
+document.querySelector('#moleculeWrapper')  .appendChild( renderer.domElement )
 
 const controls = new OrbitControls( camera, renderer.domElement )
 controls.enableDamping = true
